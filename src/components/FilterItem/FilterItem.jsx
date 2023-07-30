b1dfca9d5f0b883b3e6fe037fa7df88a4bc21d1c
SergiiMytrofanov <122700399+SergiiMytrofanov@users.noreply.github.com>
2023-07-30 22:04:12 +0300

    styles

--- src/components/FilterItem/FilterItem.jsx
+++ src/components/FilterItem/FilterItem.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import styles from './FilterItem.module.css'
 
 const Filter = ({ filter, onChange }) => {
   return (
-    <>
-      <input
+    <form className={styles.filterForm}>
+      <input className={styles.filterFormInput}
         type="text"
         name="filter"
         placeholder="Пошук за ім'ям"
         value={filter}
         onChange={onChange}
       />
-    </>
+    </form>
   );
 };
 
