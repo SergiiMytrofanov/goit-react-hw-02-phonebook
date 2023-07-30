934edcc7a38a00d6145f4bb5cdbcebc975a24d57
SergiiMytrofanov <122700399+SergiiMytrofanov@users.noreply.github.com>
2023-07-30 20:14:04 +0300

    Update App.jsx
    added functionality to add phone numbers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,31 +4,38 @@ import { nanoid } from 'nanoid';
 class App extends Component {
   state = {
     contacts: [],
-    name: ''
+    name: '',
+    number: ''
   };
 
   handleNameChange = (event) => {
     this.setState({ name: event.target.value });
   };
 
+  handleNumberChange = (event) => {
+    this.setState({ number: event.target.value });
+  };
+
   handleSubmit = (event) => {
     event.preventDefault();
-    const { name, contacts } = this.state;
-    if (name.trim() === '') return;
+    const { name, number, contacts } = this.state;
+    if (name.trim() === '' || number.trim() === '') return;
 
     const newContact = {
       id: nanoid(),
-      name: name.trim()
+      name: name.trim(),
+      number: number.trim()
     };
 
     this.setState({
       contacts: [...contacts, newContact],
-      name: ''
+      name: '',
+      number: ''
     });
   };
 
   render() {
-    const { name, contacts } = this.state;
+    const { name, number, contacts } = this.state;
 
     return (
       <div>
@@ -38,18 +45,29 @@ class App extends Component {
             type="text"
             name="name"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            title="Ім'я може містити лише літери, апостроф, тире та пробіли. Наприклад, Адріан, Джейкоб Мерсер, Шарль де Батц де Кастельмор д'Артаньян"
             required
             value={name}
             onChange={this.handleNameChange}
           />
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Номер телефону повинен складатися з цифр і може містити пробіли, тире, круглі дужки та починатися з +"
+            required
+            value={number}
+            onChange={this.handleNumberChange}
+          />
           <button type="submit">Додати контакт</button>
         </form>
-        <ul>
+        <ol>
           {contacts.map((contact) => (
-            <li key={contact.id}>{contact.name}</li>
+            <li key={contact.id}>
+              {contact.name}: &nbsp;&nbsp; {contact.number}
+            </li>
           ))}
-        </ul>
+        </ol>
       </div>
     );
   }
